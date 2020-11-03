55afa97c871724f66be421cbbccaab4436d4701d
Adam Dykhouse <awd5235@gmail.com>
2020-11-02 23:13:57 -0500

    Added comments detailing how to use git

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
-//
-//  main.cpp
-//  Synth
-//
-//  Created by Adam Dykhouse on 11/2/20.
-//
+/*==================== Using Git =====================
+ 
+ -------------------One Time Setup--------------------
+ 1. Navigate to folder containing desired source code
+ 2. "git init" to create a local repository in the .git folder by default it will be called master
+ 3. On github create a remote repository and copy it's SSH URL
+ 4. Link remote repo to the local one and give it the name origin: "git remote add origin <URL from step 3>"
+ 
+ 
+ -------------------Routine Maintenance----------------
+ 1. Make changes to source
+ 2. In terminal navigate to source file enclosing folder (Don't enter .git)
+ 3. "git add <changed file>" in this case main.cpp or simply "git add ." to add all
+ 4. Optional: "git diff" to see changes added
+ 5. git commit -m "Message detailing changes made"
+ 6. "git push origin master" changes will now be pushed to github
+*/
+
 
 #include <iostream>
 
